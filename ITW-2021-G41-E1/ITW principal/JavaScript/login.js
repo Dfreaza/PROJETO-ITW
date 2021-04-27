536e00cca0edbb9d92519f6dc6884ad84fed5333
Tiago Ferreira <82768310+tiagoferreira02@users.noreply.github.com>
2021-04-27 22:57:17 +0100

    pokemon iniciais no login
    funciona mas ainda nao esta como quero xD

--- ITW-2021-G41-E1/ITW principal/JavaScript/login.js
+++ ITW-2021-G41-E1/ITW principal/JavaScript/login.js
@@ -11,32 +11,36 @@ function handleSubmit(event){
 	const {
 		username,
 		password,
+		starter,
 		login_type,
 		result
 	} = event.target;
 
 	const processUser = login_type.value == "register" ? registerUser : loginUser;
-	const response    = processUser(username.value, password.value);
+	const response    = processUser(username.value, password.value,starter.value);
 	result.innerHTML  = response;
 }
 // registar user.................................................................
-function registerUser(username, password){
+function registerUser(username, password, starter){
 	window.localStorage.setItem("Login__username", username);
 	window.localStorage.setItem("Login__password", password);
-
-	return `New user ${username} now registered!`;
+	window.localStorage.setItem("Starter__pokemon", starter);
+	return `New user ${username} now registered! Your Pokémon is ${starter}!`;
 }
 // login......................................................................
-function loginUser(username, password){
+function loginUser(username, password, starter){
 	const registeredUser     = window.localStorage.getItem("Login__username");
 	const registeredPassword = window.localStorage.getItem("Login__password");
+	const registeredStarter = window.localStorage.getItem("Starter__pokemon");
 
 	const validUser     = username == registeredUser;
 	const validPassword = password == registeredPassword;
+	const validStarter = starter == registeredStarter;
 
-	if(validUser && validPassword){
-		return `User ${username} successfully logged-in!`;
+	if(validUser && validPassword && validStarter){
+		return `User ${username} successfully logged-in with is friend ${starter}!`;
 	}
         else if(!validUser)     return `Username ${username} has not been registered. `;
 	else if(!validPassword) return `Incorrect password for username ${username}`;
+	else if (!validStarter) return `Incorrect starter pokémon for the username ${username}`;
 }
